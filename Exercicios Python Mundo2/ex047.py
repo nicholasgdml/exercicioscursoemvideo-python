a0f9f69d5bb70d6daec8309f4a64fe170edc1163
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-07-06 13:02:44 -0300

    ex046 até 048 concluído

---
+++ Exercicios Python Mundo2/ex047.py
@@ -0,0 +1,4 @@
+# Exercício Python 47: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+for par in range(0, 51, 2):
+    print(par, end=' ')
+print('Acabou!')
