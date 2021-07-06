4c1f1f7eca96bbe8daac549d13f51385055b4ad3
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-07-06 16:41:24 -0300

    ex057 concluído

--- Exercicios Python Mundo2/ex055.py
+++ Exercicios Python Mundo2/ex055.py
@@ -1,1 +1,14 @@
 # Exercício Python 55: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+maior = 0
+menor = 0
+for c in range(1, 6):
+    peso = float(input('Digite o peso da {}ª pessoas: '.format(c)))
+    if c == 1:
+        menor = peso
+        maior = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso foi {}Kg\ne o menor foi {}Kg'.format(maior, menor))
