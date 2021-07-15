7c7fb8e9077a6c390eebca234330ccea5fd51068
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-07-15 15:43:13 -0300

    ex060 concluído

---
+++ Exercicios Python Mundo2/ex060.py
@@ -0,0 +1,11 @@
+'''Exercício Python 060: Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo:
+5! = 5 x 4 x 3 x 2 x 1 = 120'''
+from math import factorial
+n = int(input('Digite um número para fatorar: '))
+f = factorial(n)
+c = n
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    c -= 1
+print(f)
