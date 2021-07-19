82dc9aff1cf82f6b68b8bc7c05668519ba32aa24
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-07-19 12:43:14 -0300

    ex062.py concluído

--- Exercicios Python Mundo2/ex062.py
+++ Exercicios Python Mundo2/ex062.py
@@ -5,9 +5,15 @@ print('Gerador de PA')
 print('='*30)
 primeiro = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razão: '))
-c = 0
-resposta = 2
-while resposta != 0:
-    if c == 0:
-        
-    resposta = int(input(''))
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{} → '.format(primeiro), end='')
+        primeiro += razao
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados!'.format(total))
