577fbd72cf6dc6cefe4e6abeaebd5d247299e44e
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-07-28 22:23:06 -0300

    declarei a tupla do ex073

---
+++ Exercicios Python Mundo 3/ex073.py
@@ -0,0 +1,9 @@
+'''Exercício Python 73: Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
+a) Os 5 primeiros times.
+b) Os últimos 4 colocados.
+c) Times em ordem alfabética.
+d) Em que posição está o time da Chapecoense.
+'''
+
+	
+times = 'Palmeiras', 'Atlético-MG', 'Fortaleza', 'Bragantino', 'Athletico-PR', 'Flamengo', 'Ceará', 'Atlético-GO', 'Bahia', 'Corinthians', 'Fluminense', 'Santos', 'Juventude', 'Internacion, 'Cuiabá', 'Sport', 'São Paulo', 'América-MG', 'Grêmio', 'Chapecoense'
