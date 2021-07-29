bad0e6d20ee880365fa454168a70b6d8c02a8a95
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-07-28 22:11:09 -0300

    ex072 concluído

---
+++ Exercicios Python Mundo 3/ex072.py
@@ -0,0 +1,9 @@
+'''Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.'''
+
+numero_extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+while True:
+    numero = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= numero <= 20:
+        break
+    print('Tente novamente. ', end='')
+print(f'Você digitou "{numero_extenso[numero]}"')
