f236b784fa7e8d05c66229d9e1c86c0f703355b5
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-08-18 17:06:49 -0300

    ex078 versao final

--- Exercicios Python Mundo 3/ex078.py
+++ Exercicios Python Mundo 3/ex078.py
@@ -3,21 +3,21 @@
     
 lista = []
 for c in range(0, 5):
-    n = int(input(f'Digite o número para a posição {c}: '))
+    lista.append(int(input(f'Digite o número para a posição {c}: ')))
     if c == 0:
-        menor_numero = n
+        menor_numero = lista[c]
         posicao_menor_numero = c
-        maior_numero = n
+        maior_numero = lista[c]
         posicao_maior_numero = c
     else:
-        if n < menor_numero:
-            menor_numero = n
+        if lista[c] < menor_numero:
+            menor_numero = lista[c]
             posicao_menor_numero = c
-        elif n > maior_numero:
-            maior_numero = n
+        elif lista[c] > maior_numero:
+            maior_numero = lista[c]
             posicao_maior_numero = c
 
-print('-=-'*10)
+print('-=-'*13)
 print(lista)
 print(f'O maior valor digitado foi {maior_numero} na posição {posicao_maior_numero}.')
 print(f'O menor valor digitado foi {menor_numero} na posição {posicao_menor_numero}.')
