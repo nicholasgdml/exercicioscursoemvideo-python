e8107da677b9385bdf6da8c93980e45bce42f3e5
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-08-18 16:42:27 -0300

    ex078.py concluido

---
+++ Exercicios Python Mundo 3/ex078.py
@@ -0,0 +1,23 @@
+# Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
+#No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+    
+lista = []
+for c in range(0, 5):
+    n = int(input(f'Digite o número para a posição {c}: '))
+    if c == 0:
+        menor_numero = n
+        posicao_menor_numero = c
+        maior_numero = n
+        posicao_maior_numero = c
+    else:
+        if n < menor_numero:
+            menor_numero = n
+            posicao_menor_numero = c
+        elif n > maior_numero:
+            maior_numero = n
+            posicao_maior_numero = c
+
+print('-=-'*10)
+print(lista)
+print(f'O maior valor digitado foi {maior_numero} na posição {posicao_maior_numero}.')
+print(f'O menor valor digitado foi {menor_numero} na posição {posicao_menor_numero}.')
