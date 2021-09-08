f7319e33042df51a40bd6c7036734781e07af6ac
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-09-07 22:09:14 -0300

    ex096 concluído

---
+++ ex_python_mundo3/ex096.py
@@ -0,0 +1,12 @@
+'''Exercício Python 096: Faça um programa que tenha uma função chamada área(),
+que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.'''
+def area(a, b):
+    area = a * b
+    print(f'A área de um terro de {a} x {b} é de {area}m²!')
+
+
+
+print('CONTROLE DE TERRENOS')
+m1 = int(input('Largura[M]: '))
+m2 = int(input('Comprimento[M]: '))    
+area(m1, m2)
