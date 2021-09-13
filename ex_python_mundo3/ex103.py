637a5c5014d347c34c52056d0942ec26dd7fd2b9
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-09-13 08:44:57 -0300

    ex103 concluído!

--- ex_python_mundo3/ex103.py
+++ ex_python_mundo3/ex103.py
@@ -5,4 +5,16 @@ informado corretamente."""
 
 
 def ficha(jogador='<desconhecido>', gols=0):
-    
+    print(F'O jogador {jogador} fez {gols} gol(s) no campeonato.')
+
+
+n = str(input('Nome do jogador: '))
+g = str(input('Número de gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gols=g)
+else:
+    ficha(n, g)
