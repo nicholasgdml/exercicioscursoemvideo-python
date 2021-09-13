75e4f6753a63eb1525ef9628f97219c6bb2e1cee
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-09-12 22:13:23 -0300

    ex102 concluído

--- ex_python_mundo3/ex096.py
+++ ex_python_mundo3/ex096.py
@@ -1,5 +1,6 @@
 '''Exercício Python 096: Faça um programa que tenha uma função chamada área(),
 que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.'''
+
 def area(a, b):
     area = a * b
     print(f'A área de um terro de {a} x {b} é de {area}m²!')
--- ex_python_mundo3/ex102.py
+++ ex_python_mundo3/ex102.py
@@ -2,14 +2,22 @@
 O primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional)
 indicando se será mostrado ou não na tela o processo de cálculo do fatorial."""
 
-def fatorial(numero, show=True):
-    """Calcula o Fatorial de um numero
-        :param numero: É o numero a ser Fatorado
-        :param show: (opcional)  Mostra ou não a conta (Padrão True)
-        :returm: O valor fatorado de numero"""
 
-                
-# Programa Principal
-print(help(fatorial))
+def fatorial(numero, show=True):
+    """-> Função retorna um número faturado!
+        :param numero: Número a ser fatorado
+        :param show: (opcional) Retorna a conta do fatorial (padrão True)
+        :return: Retorna o número fatorado"""
+    numeroFatorado = 1
+    for c in range(numero, 0, -1):
+        if show:
+            if c != 1:
+                print(c, end=' x ')
+            else:
+                    print('1 =', end=' ')
+        numeroFatorado *= c
+    return numeroFatorado
+
+
+print(fatorial(8))
 print(fatorial(8, False))
-print(fatorial(10))
