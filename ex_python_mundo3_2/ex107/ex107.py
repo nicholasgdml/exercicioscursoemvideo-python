dca0ba9c2b6801c3d7dfb3cb1adcac706a756fa2
Nicholas Gabriel De Matos Leal <nicholasgdml12@outlook.com>
2021-09-21 10:01:54 -0300

    ex 107 quase concluído

--- ex_python_mundo3_2/ex107/ex107.py
+++ ex_python_mundo3_2/ex107/ex107.py
@@ -2,3 +2,8 @@
 que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
 Faça também um programa que importe esse módulo e use algumas dessas funções."""
 
+import moedas
+
+preco = float(input('Digite o preço: R$'))
+print(f'A metade de R${preco} é R${moedas.metade(preco)}')
+print(f'O dobro de R${preco} é R%{moedas.dobro(preco)}')
---
+++ ex_python_mundo3_2/ex107/moedas.py
@@ -0,0 +1,16 @@
+def metade(num):
+    return num / 2
+    
+
+def dobro(num):
+    return num * 2
+
+
+def aumentar(num, porcentagem=10):
+    porcentagem = num / (porcentagem/100)
+    return num + porcentagem
+
+
+def diminuir(num, porcentagem=10):
+    porcentagem = num / (porcentagem/100)
+    return num - porcentagem
